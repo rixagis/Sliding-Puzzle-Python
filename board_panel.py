56ffb03d9db4f51f24dd83d9805164e38573a3cf
Rixagis <yellowstone222@yahoo.com>
2021-03-27 18:14:18 +0300

    added docstrings in board_panel

--- board_panel.py
+++ board_panel.py
@@ -7,9 +7,29 @@ This module contains class BoardPanel - a GUI element for showing game board.
 import tkinter
 
 class BoardPanel(tkinter.Canvas):
+    """
+    UI element used to show the state of the board.
 
+    Attributes:
+        event_listener: Object listening for click events.
+            Must provide 'click(x_cell, y_cell)' method.
+        width_cells (int): Width of the board in cells.
+        height_cells (int): Height of the board in cells.
+        cell_width (int): Width of one cell in pixels.
+        cell_height (int): Height of one cell in pixels.
+    """
 
     def __init__(self, root, event_listener, width_cells, height_cells, **kwargs):
+        """Initializes a BoardPanel object.
+
+        Args:
+            root: tk root.
+            event_listener: Object listening for click events.
+                Must provide 'click(x_cell, y_cell)' method.
+            width_cells (int): Width of the board in cells.
+            height_cells (int): Height of the board in cells.
+            **kwargs: tkinter canvas kwargs.
+        """
         tkinter.Canvas.__init__(root, **kwargs)
         self.event_listener = event_listener
         self.bind("<Button-1>", self.on_click)
@@ -20,12 +40,19 @@ class BoardPanel(tkinter.Canvas):
         self.cell_height = int(self.winfo_height() / self.height_cells)
 
     def on_click(self, event):
+        """Processes canvas mouse click event and dispatches to listener.
+        """
         x_cell = int(event.x / self.cell_width)
         y_cell = int(event.y / self.cell_height)
 
         self.event_listener.click(x_cell, y_cell)
 
     def draw_game_state(self, game):
+        """Displays the state of the game in canvas.
+
+        Args:
+            game (Game): Game object which state should be diplayed.
+        """
         self.delete("all")
 
         for row in game.height:
