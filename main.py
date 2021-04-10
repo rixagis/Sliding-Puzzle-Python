7d2dbd77c054ad6308abfdcfae42cbdb633235f5
Rixagis <yellowstone222@yahoo.com>
2021-04-10 21:14:46 +0300

    Added 'shuffle' menu item, added docstrings in main.py

--- main.py
+++ main.py
@@ -4,19 +4,45 @@ from board_panel import BoardPanel
 from game import Game
 
 class Main:
+    """Utilitary class responsible for intercommunication between game, board_panel and tk root.
+
+    Attributes:
+        game (Game): Respective Game object.
+        board (BoardPanel): Respective BoardPanel object.
+        root: board's tk root object.
+    """
+
     def __init__(self, game, board, root):
+        """Initialize Main object.
+
+        Args:
+            game (Game): Respective Game object.
+            board (BoardPanel): Respective BoardPanel object.
+            root: board's tk root object.
+        """
         self.game = game
         self.board = board
         self.root = root
 
     def click(self, x, y):
+        """Process mouse click event. Interface for `board` object.
+
+        Args:
+            x (int): x coord of click, in squares.
+            y (int): y coord of click, in squares.
+        """
         if game.is_solved(): return
         self.game.press(x, y)
-        #print(self.game)
         self.board.draw_game_state(self.game)
         if game.is_solved():
-            #tk.messagebox.showinfo(message='SOLVED!')
             root.title("SOLVED!")
+
+    def shuffle(self):
+        """Shuffle the squares of `game` randomly, show the new game state in `board`.
+        """
+        self.game.shuffle()
+        root.title("Sliding Puzzle")
+        self.board.draw_game_state(self.game)
 
 root = tk.Tk()
 root.title("Sliding Puzzle")
@@ -24,8 +50,12 @@ game = Game(4, 4)
 game.shuffle()
 board = BoardPanel(root, 4, 4, width=400, height=400, bg="black")
 
+menu = tk.Menu(root)
+root.config(menu=menu)
+
 
 main = Main(game, board, root)
+menu.add_command(label='Shuffle', command=main.shuffle)
 board.add_listener(main)
 board.pack()
 board.draw_game_state(game)
